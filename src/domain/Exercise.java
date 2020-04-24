8ee8f5a6b22570754e31f4e06db7611a9ade4f56
AndreyBest <andrey255@live.com>
2020-04-24 12:14:52 +0300

    Add solution files

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,47 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+/**
+ * Class for getting static method
+ * @author Andrii Kotliar
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    /**
+     * Get get the most repeated number of an array
+     * @param arrayOfNumbers
+     * @return the most repeated number
+     * @throws java.lang.Exception
+     */
+    public static int Calculate(ArrayList<Integer> arrayOfNumbers) throws Exception {
+        if (arrayOfNumbers.size() < 1) {
+            throw new Exception("Array with less than 1 value is not supported");
+        }
+        
+        HashMap<Integer, Integer> mapOfNumbers = new HashMap();
+        for (int i = 0; i < arrayOfNumbers.size(); i++) {
+            Integer mapValue = mapOfNumbers.get(arrayOfNumbers.get(i));
+            if (mapValue == null) {
+                mapOfNumbers.put(arrayOfNumbers.get(i), 1);
+            }
+            else {
+                mapOfNumbers.put(arrayOfNumbers.get(i), mapValue + 1);
+            }
+        }
+        
+        int mostRepeatedNumber = 0;
+        int mostRepeatedNumberRepeatTimes = 0;
+        
+        for (Entry<Integer, Integer> entry : mapOfNumbers.entrySet()) {
+            Integer value = entry.getValue();
+            if (value > mostRepeatedNumberRepeatTimes) {
+                mostRepeatedNumber = entry.getKey();
+                mostRepeatedNumberRepeatTimes = value;
+            }
+            
+        }
+        return mostRepeatedNumber;
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,39 @@
 package test;
 
 import domain.Exercise;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+/**
+ * Class where all testing goes
+ * @author Andrii Kotliar
+ */
 public class TestResult {
-
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        ArrayList<Integer> arrayOfNumbers = new ArrayList();
+        Scanner in = new Scanner(System.in);
+        boolean isNotEnd = true;
+        System.out.println("Welcome to application where you can get the most repeated number ou entered!\nEnter numbers or write 'stop' in order to stop inputing new numbers.");
+        while(isNotEnd) {
+            System.out.print("Please, enter number: ");
+            String input = in.nextLine();
+            if ("stop".equals(input.toLowerCase())) {
+                isNotEnd = false;
+            }
+            else {
+                try {
+                    arrayOfNumbers.add(Integer.parseInt(input));
+                }
+                catch (NumberFormatException exception) {
+                    System.err.println("Please enter correct number");
+                }
+            }
+        }
+        try {
+            int mostRepeatedNumber = Exercise.Calculate(arrayOfNumbers);
+            System.out.println("Most repeated number in this list is: " + mostRepeatedNumber);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
